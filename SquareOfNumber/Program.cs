059f8f952ac99cb1f7c433968e74fec23a09c414
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-03 17:40:45 +0300

    Продолжаю разгадывать работу gitignore

--- SquareOfNumber/Program.cs
+++ SquareOfNumber/Program.cs
@@ -1,2 +1,9 @@
 ﻿// Вывести квадрат числа
-
+int SquareOfNumber(int number)
+{
+    number=number*number;
+    return number;
+}
+int UserNumber= Convert.ToInt32(Console.ReadLine());
+int Result = SquareOfNumber(UserNumber);
+Console.WriteLine("Ваше число в квадрате равно - {0}", Result);
