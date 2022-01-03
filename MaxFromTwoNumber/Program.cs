baad9d3dae8ddca67fd191d9df3c4e3d1f6f9ef0
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-03 19:03:50 +0300

    Использовав две функции решил задачу  с нахождением максимального из двух чисел

--- MaxFromTwoNumber/Program.cs
+++ MaxFromTwoNumber/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Даны два числа. Показать большее и меньшее число
+int EnterNumber()
+{
+    Console.Write("Введите число :");
+    int Number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(" ");
+    return Number;
+}
+int MaxItem(int number1, int number2)
+{
+    if(number1>number2) return number1;
+    else if(number1<number2) return number2;
+    else 
+    {
+        Console.WriteLine("Два числа одинаковые");
+        return number1;
+    }
+}
+int FirstNumber = EnterNumber();
+int SecondNumber = EnterNumber();
+int Max = MaxItem(FirstNumber,SecondNumber);
+Console.WriteLine("Из двух чисел максимальное - {0}", Max);
