db410cff125cecf152e6b216c3913dfb895c3454
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-04 13:03:48 +0300

    Сделал решение задачи на проверку четности числа

---
+++ EvenNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить является ли число чётным
+int EnterNumber()
+{
+    Console.Write("Введите число :");
+    int Number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(" ");
+    return Number;
+}
+void CheckEven(int Number)
+{
+    int ResultOfCheck = Number%2;
+    if(ResultOfCheck==1) Console.WriteLine("Число нечетное..");
+    else Console.WriteLine("Число четное");
+}
+int UserNumber = EnterNumber();
+CheckEven(UserNumber);
