d97af8a2b04dd7d865ea249cc3a33f53e7845aee
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-07 21:53:56 +0300

    Выполнил нахождение наибольший цифры  в случайно сгенерированном числе

---
+++ MaxDigitInLine/Program.cs
@@ -0,0 +1,28 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+double GetRandomNumber()
+{
+    double Number = new Random().Next(10,100);
+    return Number;
+}
+void GetDigit(double Number)
+{
+    double Digit1 = Number%10;
+    double Digit2 = Math.Round((Number)/10);
+    if(Digit1>Digit2)
+    {
+        Console.WriteLine(Digit1);
+    }
+    else if(Digit1<Digit2)
+    {
+        Console.WriteLine(Digit2);
+    }
+    else
+    {
+        Console.WriteLine("{0}, но обе цифры одинаковые.", Digit1);
+    }
+}
+double RandomNumber = GetRandomNumber();
+Console.Write("В числе {0} наибольшая цифра : ", RandomNumber);
+GetDigit(RandomNumber);
+
